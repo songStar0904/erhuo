7ecc3f14c030c0fe5a11e81e1d476bc8245a4b8e
songstar <1043328710@qq.com>
2018-02-24 23:10:13 +0800

    获得热搜

--- src/api/main.js
+++ src/api/main.js
@@ -1,24 +1,9 @@
 import fetch from './fetch/index.js'
-// 获取 信息
-export function get () {
+// 获取热搜
+export function get_hot (params) {
   return fetch({
-    url: 'main/get',
-    method: 'get'
-  })
-}
-// 获取反馈信息
-export function get_fmsg (params) {
-  return fetch({
-    url: 'admin/get_fmsg',
+    url: 'goods/get_hot',
     method: 'get',
     params
-  })
-}
-// 修改反馈信息
-export function edit_fmsg (data) {
-  return fetch({
-    url: 'admin/edit_fmsg',
-    method: 'post',
-    data
   })
 }
--- src/views/home/home-components/searchBox.vue
+++ src/views/home/home-components/searchBox.vue
@@ -1,30 +1,49 @@
 <template>
-	<Card :padding="8" dis-hover>
+  <Card :padding="8" dis-hover>
         <Row>
-	        <Col span="12">热搜：
-	          <Button type="text" v-for="item in hot">{{item.title}}</Button>
-	        </Col>
-	        <Col span="12">
-	        	<Input v-model="search">
-			        <Button slot="append" icon="ios-search"></Button>
-			    </Input>
-	        </Col>
-	    </Row>
+          <Col span="12">热搜：
+            <Button type="text" v-for="item in hot">{{item.search_name}}</Button>
+          </Col>
+          <Col span="12">
+            <Input v-model="search">
+              <Button slot="append" icon="ios-search"></Button>
+          </Input>
+          </Col>
+      </Row>
     </Card>
 </template>
 <script>
   export default{
-  	data () {
-  		return {
-  			hot: [{
-  				title: '英语'
-  			}, {
-  				title: '四级'
-  			}, {
-  				title: 'javascript'
-  			}],
-  			search: ''
-  		}
-  	}
+    data () {
+      return {
+        hot: [{
+          search_name: '英语'
+        }, {
+          search_name: '四级'
+        }, {
+          search_name: 'javascript'
+        }],
+        search: ''
+      }
+    },
+    mounted () {
+      this.init();
+    },
+    methods: {
+      init () {
+        this.get_hot();
+      },
+      get_hot () {
+        this.$fetch.main.get_hot({
+          num: 5
+        }).then(res => {
+          if (res.code === 200) {
+            this.hot = res.data;
+          } else {
+            this.$Message.error(res.msg);
+          }
+        })
+      }
+    }
   }
 </script>
